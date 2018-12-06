4727d7c879041bc4601fce25fc30229acc154df8
Nikhil Anand <nikhilanand1006@gmail.com>
2018-12-07 02:15:57 +0530

    new commit

---
+++ src/onlyHuff/Node.java
@@ -0,0 +1,25 @@
+package onlyHuff;
+
+@SuppressWarnings("ALL")
+public class Node {
+    Node left, right;
+    double value;
+    String character;
+
+    public Node(double value, String character) {
+        this.value = value;
+        this.character = character;
+    }
+
+    public Node(Node left, Node right) {
+        this.value = left.value + right.value;
+        character = left.character + right.character;
+        if(left.value < right.value) {
+            this.right = right;
+            this.left = left;
+        } else {
+            this.right = left;
+            this.left = right;
+        }
+    }
+}
